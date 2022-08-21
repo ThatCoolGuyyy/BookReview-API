ccd16ca4b922e46ff94827c0acef68d7de1bf2cf
ThatCoolGuyyy <goldendarmie@gmail.com>
2022-08-22 00:16:30 +0100

    no major change

--- user_app/urls.py
+++ user_app/urls.py
@@ -5,5 +5,5 @@ from user_app.views import registration_view
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
-    path('registration/', registration_view, name='registration')
+    path('registration/', registration_view.as_view(), name='registration')
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,14 +1,16 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import APIView
 from user_app.serializers import RegistrationSerializer
 from rest_framework.response import Response
 
 
-@api_view(['POST'])
-def registration_view(request):
 
-    if request.method == 'POST':
+class registration_view(APIView):
+
+    def post(self, request):
         serializer = RegistrationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+        return Response(serializer.errors)
+            
 
